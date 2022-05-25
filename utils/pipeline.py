f431274f99ac254e72c6b0309c0fbb0eee9eb104
henningorlowski <orlowskihen@googlemail.com>
2022-05-25 11:27:02 +0200

    Update

--- utils/pipeline.py
+++ utils/pipeline.py
@@ -4,23 +4,19 @@ import pickle as cPickle
 #load url_preprocessing_pipeline
 with open('assets/vectorizer_60percent', 'rb') as f:
 	vectorizer=cPickle.load(f)
-
 #load model
 with open('assets/rf_classifier_60percent', 'rb') as fid:
 	model = cPickle.load(fid)
 
 #Load Pipeline and ML-Model and use it on the given URL. 
 def model_predict(url):
-
 	if not isinstance(url,list):
 		url = [url]
 
 	#All numbers become 1 as a simplified NLP-Feature
 	url = filter_numbers(url)
-
 	#N-Gram Vectorization with trained vectorizer
 	url_vec = vectorize(url)
-
 	#prediction of classes "malicious" or "normal"
 	pred = model.predict(url_vec)[0]
 	
