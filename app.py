bfad18a119a69ebcf0158abd7d0c73b172049a3f
henningorlowski <orlowskihen@googlemail.com>
2022-05-26 14:49:34 +0200

    Update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template, redirect, url_for
+from flask import Flask, jsonify, request, render_template
 from utils.pipeline import filter_numbers, vectorize, model_predict
 from sklearn.ensemble import RandomForestClassifier 
 import pickle as cPickle
@@ -9,11 +9,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+	"""Show frontend page, that utilizes the prediction endpoint of the Flask API
+
+	Returns:
+		str: HTML-Template for the Frontend with a basic user interface.
+	"""
     # Main page
 	return render_template('index.html')	
 
 @app.route("/prediction", methods=["POST"])
 def predict():
+	"""Classify a given URL in POST for the two classes "Malicious" and "Ok". 
+
+	Returns:
+		str: JSON-String of the predicted class: str and the ML-model confidence: (float, float) for each class
+	"""
 	try:
 		#Retrieve URL from post request
 		url = [request.json['url']]
--- utils/pipeline.py
+++ utils/pipeline.py
@@ -2,14 +2,21 @@ from sklearn.feature_extraction.text import CountVectorizer
 import pickle as cPickle
 
 #load url_preprocessing_pipeline
-with open('assets/vectorizer_60percent', 'rb') as f:
+with open('assets/vectorizer_60percent', 'rb') as f: 
 	vectorizer=cPickle.load(f)
 #load model
 with open('assets/rf_classifier_60percent', 'rb') as fid:
 	model = cPickle.load(fid)
 
-#Load Pipeline and ML-Model and use it on the given URL. 
 def model_predict(url):
+	"""Load Pipeline and ML-Model and use it to predict the class [maliciour or ok] of the given URL. 
+
+	Args:
+		url (str): URL that will be predicted.
+
+	Returns:
+		rtype: (str, float)
+	"""
 	if not isinstance(url,list):
 		url = [url]
 
@@ -24,21 +31,35 @@ def model_predict(url):
 	pred_prob = model.predict_proba(url_vec)[0]
 	return pred, pred_prob
 
-#simplify digits within the url: [0+9] is returned as 1
-def filter_numbers(data):
-	data = [item.replace('0', '1') for item in data]
-	data = [item.replace('2', '1') for item in data]
-	data = [item.replace('3', '1') for item in data]
-	data = [item.replace('4', '1') for item in data]
-	data = [item.replace('5', '1') for item in data]
-	data = [item.replace('6', '1') for item in data]
-	data = [item.replace('7', '1') for item in data]
-	data = [item.replace('8', '1') for item in data]
-	data = [item.replace('9', '1') for item in data]
-	return data
 
-#apply count-vectorizer for url
+def filter_numbers(url):
+	"""Simplify digits within an URL: [0+9] is returned as 1
+
+	Args:
+		url (str): The URL which shall be simplified.
+
+	Returns:
+		str: Simplified URL without other numbers than 1.
+	"""
+	url = [item.replace('0', '1') for item in url]
+	url = [item.replace('2', '1') for item in url]
+	url = [item.replace('3', '1') for item in url]
+	url = [item.replace('4', '1') for item in url]
+	url = [item.replace('5', '1') for item in url]
+	url = [item.replace('6', '1') for item in url]
+	url = [item.replace('7', '1') for item in url]
+	url = [item.replace('8', '1') for item in url]
+	url = [item.replace('9', '1') for item in url]
+	return url
+
 def vectorize(url):
+	"""Apply count-vectorization for an URL
+
+	Args:
+		url (str): URL that will be vectorized
+	Returns:
+		np.int64: Numpy-Array with the Count-Vectorized URL.
+	"""
 	if not isinstance(url,list):
 		url = [url]
 
